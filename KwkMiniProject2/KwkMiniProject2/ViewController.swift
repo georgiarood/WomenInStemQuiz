e7421d427345a148e58afeef19b9ca1e429c38ec
georgiarood <51422481+georgiarood@users.noreply.github.com>
2020-08-06 14:58:23 -0400

    KWK Mini Project #2

---
+++ KwkMiniProject2/KwkMiniProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KwkMiniProject2
+//
+//  Created by Apple on 8/6/20.
+//  Copyright © 2020 Georgia Rood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
